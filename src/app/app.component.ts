111cb1bbc17726f76b842acfb4101c79ec08b6ed
Roman Fok <roman.fok@kindgeek.com>
2021-08-05 00:21:52 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Output} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import { EventEmitter } from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {catchError, debounceTime, distinctUntilChanged, switchMap} from "rxjs/operators";
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testApp';
+
+  public form: FormControl = new FormControl();
+  public list: string[]  | any[] = [];
+  @Output() public onSelect = new EventEmitter();
+
+  constructor(private httpClient: HttpClient) {
+    this.form.valueChanges
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(500),
+        switchMap((search: string) => this.getList(search)),
+        catchError(() => of([]))
+      )
+      .subscribe((list) => this.list =list)
+  }
+
+  public getList(query: string): Observable<string[]> {
+    const params = new HttpParams();
+    if (query) {
+      params.append('q', query)
+    }
+    return this.httpClient.get<string[]>('https://example.com/api/items', { params });
+  }
 }
